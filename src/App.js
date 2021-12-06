46526a8a242526b908e920c3c8f61f923f38e473
Verity Tsai <veritymissed@gmail.com>
2021-12-06 12:25:01 +0800

    feat(navbar): style of the navebar button with active style dedpends on current path

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import DeleteIcon from '@mui/icons-material/Delete';
 import Cookies from 'js-cookie';
 import * as _ from 'lodash';
+import headerIcon from './static/images/gengar.png';
 
 import request from 'request';
 
@@ -314,7 +315,7 @@ function RegisterForm(props){
       if(isLoading) return;
       setIsLoading(true)
       let res = await registerPromise;
-      console.log('login res', res)
+      console.log('register res', res)
     } catch (e) {
       console.log(e)
     } finally {
@@ -368,31 +369,70 @@ function NavBar(props){
   const { userState, dispatch } = useContext(UserContext);
   console.log('userState in NavBar', userState);
   let navigate = useNavigate();
+  const currentPath = useLocation().pathname;
+
+  let color_purple = "#5052c9";
+  let color_white = "#f5f6f7";
+  let font_size = "16px";
+  let font_weight = "700";
+
+  let useStyles = makeStyles({
+    navbar_box: {
+      display: "flex",
+      padding: "8px 16px 8px 16px",
+      backgroundColor: "rgb(36, 37, 38)",
+      color: "#f5f6f7",
+      fontSize: "16px",
+    },
+    active_button:{
+      color: color_purple,
+      fontSize: font_size,
+      fontWeight: font_weight,
+    },
+    non_active_button: {
+      color: color_white,
+      fontSize: "14px",
+    },
+  });
+
+
+  const classes = useStyles();
 
   return (
-    <Box>
-      <Box>FST PokemonTcg</Box>
-      <Button onClick={(e)=>{
-        navigate('/')
-      }}>FST PokemonTcg</Button>
+    <Box className={classes.navbar_box}>
+      <Box sx={{flexGrow: 1}}>
+        <Button onClick={(e)=>{
+          navigate('/')
+        }}><img height="30" src={headerIcon}/>
+          <Box className={(currentPath === "/") ? classes.active_button : classes.non_active_button}>FST Pokémon Tcg</Box>
+        </Button>
+      </Box>
+
       {!userState.isLogin && (
-        <React.Fragment>
+        <Box>
         <Button onClick={(e)=>{
           navigate('/register')
-        }}>SignUp</Button>
+        }}>
+        <Box className={(currentPath === "/register") ? classes.active_button : classes.non_active_button}>Register</Box>
+        </Button>
         <Button onClick={(e) => {
           navigate('/login')
-        }}>Login</Button>
-        </React.Fragment>
+        }}>
+        <Box className={(currentPath === "/login") ? classes.active_button : classes.non_active_button}>Login</Box>
+        </Button>
+        </Box>
       )}
       {userState.isLogin && (
-        <React.Fragment>
-        <Button onClick={(e) => {navigate('/favorite_list')}}>Favorite list<FavoriteIcon></FavoriteIcon></Button>
+        <Box>
+        <Button onClick={(e) => {navigate('/favorite_list')}}>
+          <Box display="flex" className={(currentPath === "/favorite_list") ? classes.active_button : classes.non_active_button}><Box style={{lineHeight: "10px", margin: "0px 3px"}}><FavoriteIcon></FavoriteIcon></Box></Box>
+        </Button>
         <Button onClick={(e)=>{
           navigate('/logout');
-        }}>Logout</Button>
-        <Box>{userState.currentUserEmail}</Box>
-        </React.Fragment>
+        }}>
+          <Box className={classes.non_active_button}>Logout - {userState.currentUserEmail}</Box>
+        </Button>
+        </Box>
       )}
     </Box>
   );
